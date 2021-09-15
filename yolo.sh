38f02fde596a483bb7c1e057572fea8813b10878
ansuz <ansuz@transitiontech.ca>
2021-09-15 09:25:37 +0530

    oops. properly fix the deploy script

--- yolo.sh
+++ yolo.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 git push ssh://cryptpad@blog.cryptpad.org/home/cryptpad/blog.git
-ssh cryptpad@blog.cryptpad.org 'cd ~/blog/ && git pull'
+ssh cryptpad@blog.cryptpad.org 'cd ~/blog.cryptpad.org/ && git pull'
