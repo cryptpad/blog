91a7a42fc84ba0088d5446fc7ebf1acf723694fc
David Benque <david.benque@xwiki.com>
2022-11-02 17:04:48 +0000

    Add support for LESS

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,28 @@
+const less = require('less');
+const path = require('path');
+var fs = require( 'fs' );
 
 module.exports = (function(eleventyConfig) {
+
+    // Compile LESS into CSS
+    const inputFile = path.join(__dirname, '/_includes/LESS/main.less')
+    const outputFile = path.join(__dirname, '/public/style.css')
+
+    fs.readFile( inputFile ,function(error,data){
+        data = data.toString();
+
+        less.render(data, {
+            paths: [ inputFile ]
+        },function (e, css) {
+            fs.writeFile( outputFile, css.css, function(err){
+                if( err ){
+                    console.log(err );
+                }
+                console.log('LESS compiled');
+            });
+        });
+    });
+
     // Copy /images to /public
     eleventyConfig.addPassthroughCopy('images');
 });
