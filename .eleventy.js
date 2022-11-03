45dfb8fc57844ba525eb509900ebc699b56ff119
David Benque <david.benque@xwiki.com>
2022-11-03 15:56:11 +0000

    add fonts

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,13 @@
-
+const { DateTime } = require("luxon");
 
 module.exports = (function(eleventyConfig) {
-
-
-
     // Copy /images to /public
     eleventyConfig.addPassthroughCopy('images');
+    eleventyConfig.addPassthroughCopy({'source/fonts/': 'fonts/'})
+
+    // Filter to display dates
+    eleventyConfig.addFilter("postDate", (dateObj) => {
+        // see Luxon ref to change format https://moment.github.io/luxon/#/formatting
+        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+    });
 });
