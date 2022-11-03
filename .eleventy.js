6d7e789c15f0fa4345e610f69d00d6b8649409fd
David Benque <david.benque@xwiki.com>
2022-11-03 20:18:42 +0000

    add atom RSS feed

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,16 @@
 const { DateTime } = require("luxon");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
 
 module.exports = (function(eleventyConfig) {
+    eleventyConfig.addPlugin(pluginRss);
     // Copy /images to /public
     eleventyConfig.addPassthroughCopy('images');
     eleventyConfig.addPassthroughCopy({'source/fonts/': 'fonts/'})
+
+    // Debug filter
+    eleventyConfig.addFilter("log", (d) => {
+        console.log(d);
+      });
 
     // Filter to display dates
     eleventyConfig.addFilter("postDate", (dateObj) => {
