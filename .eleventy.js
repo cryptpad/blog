62d6bf4efaa23fa8910950736cfd13bf457237e3
David Benque <david.benque@xwiki.com>
2022-11-10 09:15:36 +0000

    add top tags + refactor layout with grid

--- .eleventy.js
+++ .eleventy.js
@@ -18,4 +18,15 @@ module.exports = (function(eleventyConfig) {
         // see Luxon ref to change format https://moment.github.io/luxon/#/formatting
         return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
     });
+
+    eleventyConfig.addFilter("topTags", (d) => {
+        let tags = [];
+        for (const [key, value] of Object.entries(d)) {
+            tags.push({name: key, count: value.length});
+        };
+        tags.sort((a, b) => parseFloat(b.count) - parseFloat(a.count));
+        tags.shift(); // remove first item "all"
+        tags = tags.slice(0, 10); // take top 10
+        return tags;
+    });
 });
